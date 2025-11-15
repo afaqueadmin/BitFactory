160f764705be1f1d47322910b25c071f48541234
hamza <hamza.najeeb455@gmail.com>
2025-11-15 17:21:21 +0500

    add wallet_address to user table

---
+++ prisma/migrations/20251115121417_add_wallet_address_field/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "public"."users" ADD COLUMN     "walletAddress" TEXT;
+
+-- Add constraint: walletAddress must be between 26 and 35 characters (alphanumeric)
+ALTER TABLE "public"."users" ADD CONSTRAINT "walletAddress_length_check"
+  CHECK (
+    "walletAddress" IS NULL OR
+    (LENGTH("walletAddress") >= 26 AND LENGTH("walletAddress") <= 35)
+  );
+
