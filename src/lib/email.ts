2042f3efdf67ea872553a2b35a34b75d3922cba5
hamza <hamza.najeeb455@gmail.com>
2026-01-27 16:03:54 +0500

    update chromium executablePath

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -313,7 +313,7 @@ export const generatePDFFromHTML = async (
         "--disable-gpu",
       ],
       executablePath: await chromium.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`,
+        `https://github.com/Sparticuz/chromium/releases/download/v143.0.4/chromium-v143.0.4-pack.tar.br`,
       ),
     });
 
