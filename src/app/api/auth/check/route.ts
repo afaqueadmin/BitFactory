ee298ccb5226be87ed337f61b025dfaad5ff4834
hamza <hamza.najeeb455@gmail.com>
2026-06-29 19:13:48 +0500

    revert. prod stop working

--- src/app/api/auth/check/route.ts
+++ src/app/api/auth/check/route.ts
@@ -38,7 +38,7 @@ export async function GET(request: NextRequest) {
       } catch (error) {
         console.error("Access token verification failed:", error);
         // Fall through to refresh token check
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        // return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         // Do not fall through to refresh token if access token is invalid, just return 401
       }
     }
